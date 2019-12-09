22b9a92f0ed18dd817db622ca38f17a4f285bc00
Andrew <wty.andrew@gmail.com>
2019-12-09 21:26:41 +0800

    episode 4,5

--- src/index.js
+++ src/index.js
@@ -63,4 +63,85 @@ function ep3() {
     __PS_MV_REG = [];
     return render();
 };
-window.onload = ep3;
+/** Circles, Ellipses and Lissajous Curves */
+function ep4() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    var centerX = width / 2;
+    var centerY = height / 2;
+    var xRadius = width / 6;
+    var yRadius = height / 3;
+    var xAngle = 0;
+    var yAngle = 0;
+    var xSpeed = 0.1;
+    var ySpeed = 0.131;
+    var x = null;
+    var y = null;
+    var render = function () {
+        ctx.clearRect(0, 0, width, height);
+        x = centerX + Math.cos(xAngle) * xRadius;
+        y = centerY + Math.sin(yAngle) * yRadius;
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2, false);
+        ctx.fill();
+        xAngle += xSpeed;
+        yAngle += ySpeed;
+        __PS_MV_REG = [];
+        return requestAnimationFrame(render);
+    };
+    __PS_MV_REG = [];
+    return render();
+};
+/** Arctangent */
+function ep5() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+    var centerX = width / 2;
+    var centerY = height / 2;
+    var arrowX = width / 2;
+    var arrowY = height / 2;
+    var xAngle = 0;
+    var yAngle = 0;
+    var xSpeed = 0.01;
+    var ySpeed = 0.02;
+    var dx = null;
+    var dy = null;
+    var angle = 0;
+    var render = function () {
+        arrowX = centerX + Math.cos(xAngle) * (width / 6);
+        arrowY = centerY + Math.sin(yAngle) * (height / 3);
+        ctx.clearRect(0, 0, width, height);
+        ctx.save();
+        ctx.translate(arrowX, arrowY);
+        ctx.rotate(angle);
+        ctx.beginPath();
+        ctx.moveTo(20, 0);
+        ctx.lineTo(-20, 0);
+        ctx.moveTo(20, 0);
+        ctx.lineTo(10, -10);
+        ctx.moveTo(20, 0);
+        ctx.lineTo(10, 10);
+        ctx.stroke();
+        ctx.restore();
+        xAngle += xSpeed;
+        yAngle += ySpeed;
+        __PS_MV_REG = [];
+        return requestAnimationFrame(render);
+    };
+    document.body.addEventListener('mousemove', function (evt) {
+        dx = evt.clientX - arrowX;
+        dy = evt.clientY - arrowY;
+        return angle = Math.atan2(dy, dx);
+    });
+    __PS_MV_REG = [];
+    return render();
+};
+window.onload = ep5;
