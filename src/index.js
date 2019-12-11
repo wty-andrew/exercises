d8e8cf587d5878ce52df85494f7641db97525b29
Andrew <wty.andrew@gmail.com>
2019-12-11 20:40:00 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
+if ('undefined' === typeof CANVAS) {
+    var CANVAS = document.getElementById('canvas');
+};
+if ('undefined' === typeof CTX) {
+    var CTX = CANVAS.getContext('2d');
+};
+if ('undefined' === typeof WIDTH) {
+    var WIDTH = window.innerWidth;
+};
+if ('undefined' === typeof HEIGHT) {
+    var HEIGHT = window.innerHeight;
+};
+CANVAS.width = WIDTH;
+CANVAS.height = HEIGHT;
+var width = WIDTH;
+var height = HEIGHT;
 /** Introduction */
 function ep1() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
     for (var _ = 0; _ < 100; _ += 1) {
-        ctx.beginPath();
-        ctx.moveTo(Math.random() * width, Math.random() * height);
-        ctx.lineTo(Math.random() * width, Math.random() * height);
-        ctx.stroke();
+        CTX.beginPath();
+        CTX.moveTo(Math.random() * width, Math.random() * height);
+        CTX.lineTo(Math.random() * width, Math.random() * height);
+        CTX.stroke();
     };
 };
 /** Intro to Trigonometry */
 function ep2() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
-    ctx.translate(0, height / 2);
-    ctx.scale(1, -1);
+    CTX.translate(0, height / 2);
+    CTX.scale(1, -1);
     var angle = 0;
     for (; angle < Math.PI * 2; ) {
-        ctx.fillRect(angle * (width / (2 * Math.PI)), Math.sin(angle) * (width / (2 * Math.PI)), 5, 5);
-        var _js1 = angle + 0.01;
-        angle = _js1;
+        CTX.fillRect(angle * (width / (2 * Math.PI)), Math.sin(angle) * (width / (2 * Math.PI)), 5, 5);
+        var _js19 = angle + 0.01;
+        angle = _js19;
     };
 };
 /** More Trigonometry */
 function ep3() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
     var centerY = height * 0.5;
     var centerX = width * 0.5;
     var baseRadius = 50;
@@ -51,11 +49,11 @@ function ep3() {
         y = centerY + Math.sin(angle) * offsetHeight;
         radius = baseRadius + Math.sin(angle / 2) * offsetRadius;
         alpha = baseAlpha + Math.sin(angle / 2) * offsetAlpha;
-        ctx.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')';
-        ctx.clearRect(0, 0, width, height);
-        ctx.beginPath();
-        ctx.arc(centerX, y, radius, 0, Math.PI * 2, false);
-        ctx.fill();
+        CTX.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')';
+        CTX.clearRect(0, 0, WIDTH, HEIGHT);
+        CTX.beginPath();
+        CTX.arc(centerX, y, radius, 0, Math.PI * 2, false);
+        CTX.fill();
         angle += speed;
         __PS_MV_REG = [];
         return requestAnimationFrame(render);
@@ -65,12 +63,6 @@ function ep3() {
 };
 /** Circles, Ellipses and Lissajous Curves */
 function ep4() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
     var centerX = width / 2;
     var centerY = height / 2;
     var xRadius = width / 6;
@@ -82,12 +74,12 @@ function ep4() {
     var x = null;
     var y = null;
     var render = function () {
-        ctx.clearRect(0, 0, width, height);
+        CTX.clearRect(0, 0, WIDTH, HEIGHT);
         x = centerX + Math.cos(xAngle) * xRadius;
         y = centerY + Math.sin(yAngle) * yRadius;
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2, false);
-        ctx.fill();
+        CTX.beginPath();
+        CTX.arc(x, y, 10, 0, Math.PI * 2, false);
+        CTX.fill();
         xAngle += xSpeed;
         yAngle += ySpeed;
         __PS_MV_REG = [];
@@ -98,12 +90,6 @@ function ep4() {
 };
 /** Arctangent */
 function ep5() {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    canvas.width = width;
-    canvas.height = height;
     var centerX = width / 2;
     var centerY = height / 2;
     var arrowX = width / 2;
@@ -116,21 +102,21 @@ function ep5() {
     var dy = null;
     var angle = 0;
     var render = function () {
+        CTX.clearRect(0, 0, WIDTH, HEIGHT);
         arrowX = centerX + Math.cos(xAngle) * (width / 6);
         arrowY = centerY + Math.sin(yAngle) * (height / 3);
-        ctx.clearRect(0, 0, width, height);
-        ctx.save();
-        ctx.translate(arrowX, arrowY);
-        ctx.rotate(angle);
-        ctx.beginPath();
-        ctx.moveTo(20, 0);
-        ctx.lineTo(-20, 0);
-        ctx.moveTo(20, 0);
-        ctx.lineTo(10, -10);
-        ctx.moveTo(20, 0);
-        ctx.lineTo(10, 10);
-        ctx.stroke();
-        ctx.restore();
+        CTX.save();
+        CTX.translate(arrowX, arrowY);
+        CTX.rotate(angle);
+        CTX.beginPath();
+        CTX.moveTo(20, 0);
+        CTX.lineTo(-20, 0);
+        CTX.moveTo(20, 0);
+        CTX.lineTo(10, -10);
+        CTX.moveTo(20, 0);
+        CTX.lineTo(10, 10);
+        CTX.stroke();
+        CTX.restore();
         xAngle += xSpeed;
         yAngle += ySpeed;
         __PS_MV_REG = [];
